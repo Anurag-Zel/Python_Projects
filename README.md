dc7157d60f0e677ef79e4dd6ef78165b823901ec
Anurag-Zel <lightningphoenix484@gmail.com>
2025-09-15 13:12:32 +0530

    add readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Assignments - By Anurag Majumdar
+
+## Task List
+* ### [Assignment 1](https://github.com/Anurag-Zel/Python_Projects/tree/main/assignment1) 
--- assignment1/task2.py
+++ assignment1/task2.py
@@ -5,7 +5,8 @@ try :
     if len(fname) == 0 and len(lname) == 0:
         raise Exception("\n\nError : Enter atleast one character")
     else :
-        print(f"\n\nHello, {fname + ' ' + lname}! Welcome to the Python program")    
+        concat_name = fname + " " + lname
+        print(f"\n\nHello, {concat_name}! Welcome to the Python program")    
 except Exception as e :
     print(f"\n Error: {e}")
 finally : 
