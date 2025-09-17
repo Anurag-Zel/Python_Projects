ff35dceed4f2d9d2120d7a887d89cbc692056413
Anurag-Zel <lightningphoenix484@gmail.com>
2025-09-17 21:58:46 +0530

    assignment_4_task_1

---
+++ assignment4/task1.py
@@ -0,0 +1,16 @@
+import os
+
+file_name = 'sample.txt'
+relative_path = 'assignment4/' + file_name
+try :
+    if os.path.exists(relative_path) : 
+        read_file = open(relative_path, 'rt')
+        print("\nReading file content:")
+        print(f"\nLine 1: {read_file.readline()}")
+        print(f"Line 2: {read_file.readline()}")
+    else : 
+        raise FileNotFoundError    
+except Exception as e:
+    print(f"\nError: The file {file_name} was not found.")
+finally : 
+    print("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")        
